43693b6d614fe85749037eacf6b92e7d35461e59
安慕希 <94945381+anmuxi513@users.noreply.github.com>
2023-03-09 10:15:06 +0800

    Add files via upload

---
+++ vision/src/views/MapPage.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="com-page">
+      <Map></Map>
+    </div>
+  </template>
+  
+  <script>
+  import Map from "@/components/Map.vue";
+  export default {
+    components: {
+      Map,
+    },
+    data() {
+      return {};
+    },
+    methods: {},
+  };
+  </script>
+  
+  <style lang="less" scoped>
+  </style>
---
+++ vision/src/views/RankPage.vue
@@ -0,0 +1,26 @@
+<!--
+针对于 /rankpage 这条路径而显示出来的
+在这个组件中, 通过子组件注册的方式, 要显示出Rank.vue这个组件
+-->
+<template>
+  <div class="com-page">
+    <Rank></Rank>
+  </div>
+</template>
+  
+  <script>
+import Rank from "@/components/Rank";
+export default {
+  data() {
+    return {};
+  },
+  methods: {},
+  components: {
+    Rank,
+  },
+};
+</script>
+  
+  <style lang="less" scoped>
+</style>
+  
